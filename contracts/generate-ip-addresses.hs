678f2063b52dd8cf7b5e442cbe8a855f5229ae0c
Jannis Geuppert <jannis.n.geuppert@stud.h-da.de>
2022-03-15 23:48:30 +0100

    Added generate-ip-addresses.hs

---
+++ contracts/generate-ip-addresses.hs
@@ -0,0 +1,20 @@
+-- TODO: Remove "" from output, rename variables
+import System.Environment ( getArgs )
+import Data.List ( intercalate )
+
+splitAt' :: Int -> [a] -> [[a]]
+splitAt' n l = [take n l, drop n l]
+
+isValidNumber :: String -> Bool
+isValidNumber x = read x >= 0 && read x <= 255 && not (head x == '0' && read x > 0)
+
+consume :: [String] -> [[String]]
+consume ip = map ((init ip ++) . (\x -> splitAt' x (last ip))) [1..3]
+
+generateIPAdresses :: String -> [String]
+generateIPAdresses n = map (intercalate ".") $ filter (all isValidNumber) $ consume (return n) >>= consume >>= consume
+
+main :: IO ()
+main = do
+    n <- generateIPAdresses . head <$> getArgs
+    putStrLn $ filter (/= '"') $ show n
