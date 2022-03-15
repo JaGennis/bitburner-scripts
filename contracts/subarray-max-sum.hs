0e91a991586e7c074f13af84edb8217104977697
Jannis Geuppert <jannis.n.geuppert@stud.h-da.de>
2022-03-15 23:49:37 +0100

    Added subarray-max-sum.hs

---
+++ contracts/subarray-max-sum.hs
@@ -0,0 +1,6 @@
+import Data.List ( inits )
+import System.Environment ( getArgs )
+
+main = do
+    array <- read . head <$> getArgs
+    print $ maximum $ map sum $ concatMap (inits . (`drop` array)) [0..length array - 1]
