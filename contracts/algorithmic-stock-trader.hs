3b3f8f8891361d4a30e6f2971f09a3644451af10
Jannis Geuppert <jannis.n.geuppert@stud.h-da.de>
2022-03-22 00:15:31 +0100

    Made much faster draft of algorithmic-stock-trader.hs

--- contracts/algorithmic-stock-trader.hs
+++ contracts/algorithmic-stock-trader.hs
@@ -1,14 +1,38 @@
+import Control.Arrow      ( (&&&), first )
+import Data.Bool          ( bool )
 import Data.List          ( subsequences )
 import System.Environment ( getArgs )
 import Control.Parallel.Strategies
-import Data.Bool ( bool )
 
-buySell :: [Int] -> Int
-buySell [] = 0
-buySell l = (negate . head) l + (l !! 1) + buySell (drop 2 l)
+type BuySellPairs = [(Int, Int)]
+type PriceList = [Int]
+
+head' [] = -1000
+head' l = head l
+
+getSellPrice :: Int -> PriceList -> [(BuySellPairs,PriceList)]
+getSellPrice _ [] = []
+getSellPrice buyPrice prices = newItem ++ getSellPrice buyPrice restPrices
+    where (restPrices, sellPrice) = drop 1 &&& head' $ dropWhile (<= buyPrice) prices
+          --newItem = [([(buyPrice, sellPrice)],restPrices) | not . null $ restPrices]
+          newItem = [([(buyPrice, sellPrice)],restPrices)]
+
+findBuySellPair :: PriceList -> [(BuySellPairs, PriceList)]
+findBuySellPair prices = concatMap (\x -> getSellPrice (prices !! x) (drop (succ x) prices)) [0..pred . length $ prices]
+
+buildNbsPairs :: Int -> PriceList -> [(BuySellPairs, PriceList)]
+buildNbsPairs 0     priceList = return ([], priceList)
+buildNbsPairs trans priceList = buildNbsPairs (pred trans) priceList >>= mmm
+    where mmm (pairs,priceList) = map (first (pairs ++)) $ findBuySellPair priceList
+
+maxProfit :: Int -> PriceList -> Int
+maxProfit n pl = maximum $ map (negate. sum . map (uncurry (-)) . fst) $ concatMap (\x -> buildNbsPairs x pl) w
+    --where w = if n == -1 then [1..div (length pl) 2] else [n]
+    --where w = [1..div (length pl) 2]
+    where w = [1..n]
 
 main = do
-    stocks <- read . (!! 1) <$> getArgs
-    trans <- (\x -> bool x (div (length stocks) 2) (x == -1)) . read .  head  <$> getArgs
-    let profits = map buySell $ filter (\sublist -> length sublist <= trans * 2 && (even . length) sublist) $ subsequences stocks
-    print $ maximum (profits `using` parBuffer 8192 rdeepseq)
+    trans  <- read . head <$> getArgs
+    stocks <- read . last <$> getArgs
+    print $ maxProfit trans stocks
+    --print $ buildNbsPairs trans stocks
