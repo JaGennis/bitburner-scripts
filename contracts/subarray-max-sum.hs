75e228bb3154d7f467400b40fe0ad13f325504dc
Jannis Geuppert <jannis.n.geuppert@stud.h-da.de>
2022-03-23 16:39:12 +0100

    Simpler input read in subarray-max-sum.hs

--- contracts/subarray-max-sum.hs
+++ contracts/subarray-max-sum.hs
@@ -3,5 +3,5 @@ import Data.List ( inits )
 import System.Environment ( getArgs )
 
 main = do
-    array <- read . head <$> getArgs
+    array <- read . (++ "]") . ("[" ++ ) . head <$> getArgs
     print $ maximum $ map sum $ concatMap (inits . (`drop` array)) [0..length array - 1]
