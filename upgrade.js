32ac273bb13bd04f66e47164e7447e48a065b846
JaGennis <JaGennis@users.noreply.github.com>
2022-03-29 23:45:19 +0200

    Refactored upgrade.js

--- upgrade.js
+++ upgrade.js
@@ -1,31 +1,30 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 
-	var serverName = "hwgw"
+	const serverName = "hwgw"
 
 	while (true) {
 
 		while (ns.getPurchasedServers().length < ns.getPurchasedServerLimit()) {
-			var ram = 2
-			while(ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram*2) && ram <= 2048)
+			let ram = 8
+			while (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram * 2) && ram <= 2048)
 				ram *= 2
 			ns.purchaseServer(serverName, ram)
 			await ns.sleep(1000)
 		}
-		
-		for (var i = 0; i < ns.getPurchasedServers().length; i++){
-			var ram = ns.getServer(ns.getPurchasedServers()[i]).maxRam * 2
-			while(ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram*2))
+
+		for (let server of ns.getPurchasedServers()) {
+			let ram = ns.getServer(server).maxRam * 2
+			while (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram * 2))
 				ram *= 2
-			if(ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)){
-				var files = ns.ls(ns.getPurchasedServers()[i])
-				// wait for script to finish
-				ns.killall(ns.getPurchasedServers()[i])
-				ns.deleteServer(ns.getPurchasedServers()[i])
-				await ns.sleep(1000)
+			if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) {
+				const files = ns.ls(server)
+				while (ns.ps(server).length > 0)
+					await ns.sleep(100)
+				ns.deleteServer(server)
 				ns.purchaseServer(serverName, ram)
 				if (files.length > 0)
-					await ns.scp(files, ns.getPurchasedServers()[i])
+					await ns.scp(files, server)
 			}
 		}
 		await ns.sleep(1000)
