28d8c3360b82e2f30270a9d004ff6fcdef9b1329
Jannis Geuppert <jannis.n.geuppert@stud.h-da.de>
2022-03-29 16:18:42 +0200

    Minor refactoring in find-all-valid-math-expressions.hs

--- contracts/find-all-valid-math-expressions.hs
+++ contracts/find-all-valid-math-expressions.hs
@@ -1,8 +1,7 @@
 #!/usr/bin/env runhaskell
-import Data.List
+import Data.Char          ( isDigit )
+import Data.List          ( elemIndices, nub, permutations )
 import System.Environment ( getArgs )
-import Data.Char ( isDigit )
-import Control.Arrow
 
 ps :: [[[Int]]]
 ps = [] : map parts [1..]
@@ -18,38 +17,39 @@ permutateOrder elements = map (\x -> consume x elements) (nub $ concatMap permut
 
 mathOps = ['+', '-', '*']
 
-rofl :: [Char] -> String -> [Int] -> [String]
-rofl ops str [] = [str]
-rofl ops "" [x] = [show x]
-rofl ops "" (x:xs:xss) = concatMap (\f -> rofl ops (show x ++ [f] ++ show xs) xss) ops
-rofl ops str (x:xs) = concatMap (\f -> rofl ops (str ++ [f] ++ show x) xs) ops
+formMathExpressions :: [Char] -> String -> [Int] -> [String]
+formMathExpressions ops str [] = [str]
+formMathExpressions ops "" [x] = [show x]
+formMathExpressions ops "" (x:xs:xss) = concatMap (\f -> formMathExpressions ops (show x ++ [f] ++ show xs) xss) ops
+formMathExpressions ops str (x:xs) = concatMap (\f -> formMathExpressions ops (str ++ [f] ++ show x) xs) ops
 
 data Token = Number Int | Plus | Minus | Multiply
     deriving (Show, Eq)
 
 lexer :: String -> [Token]
-lexer "" = []
-lexer ('+':str) = Plus : lexer str
-lexer ('-':str) = Minus : lexer str
+lexer ""        = []
+lexer ('+':str) = Plus     : lexer str
+lexer ('-':str) = Minus    : lexer str
 lexer ('*':str) = Multiply : lexer str
-lexer str = Number (read $ takeWhile isDigit str) : lexer (dropWhile isDigit str)
+lexer       str = Number (read $ takeWhile isDigit str)
+                    : lexer (dropWhile isDigit str)
 
 eval :: [Token] -> Int
-eval [] = 0
-eval [Number n] = n
+eval []                 = 0
+eval [Number n]         = n
 eval str
     | elem Multiply str = eval $ init front ++ [ Number (fac1 * fac2) ] ++ tail back
-    | otherwise = eval $ Number (op firstNumber secondNumber) : drop 3 str
+    | otherwise         = eval $ Number (op firstNumber secondNumber) : drop 3 str
     where (front,_:back) = break (== Multiply) str
           Number fac1 = last front
           Number fac2 = head back
           op = case str !! 1 of
             Plus -> (+)
             Minus -> (-)
-          Number firstNumber = head str
+          Number firstNumber  = head str
           Number secondNumber = str !! 2
 
 main = do
     (digits, _:target) <- break (==',') . filter (\x -> x /= '[' && x /= ']' && x /= '"') . head <$> getArgs
-    let calcs = concatMap (rofl mathOps "" . map read) $ permutateOrder digits
-    putStrLn $ filter (/= '"') $ show $ map (calcs !!) $ elemIndices (read target) $ map (eval.lexer) calcs
+    let mathExpressions = concatMap (formMathExpressions mathOps "" . map read) $ permutateOrder digits
+    putStrLn $ filter (/= '"') $ show $ map (mathExpressions !!) $ elemIndices (read target) $ map (eval.lexer) mathExpressions
