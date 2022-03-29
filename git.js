c4c7a9e69b4e306b385eddcc28a18f8d559fa167
Jannis Geuppert <jannis.n.geuppert@stud.h-da.de>
2022-03-29 23:07:39 +0200

    Simplified git.js

--- git.js
+++ git.js
@@ -1,19 +1,9 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/hacknet.js", "hacknet.js")
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/helper-functions.js", "helper-functions.js")
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/propagate.js", "propagate.js")
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/routine.js", "routine.js")
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/stabilize.js", "stabilize.js")
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/upgrade.js", "upgrade.js")
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/contracts.js", "contracts.js")
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/backdoor.js", "backdoor.js")
+	const scripts = [ "backdoor", "contracts", "git", "hacknet",
+        "helper-functions", "hwgw", "propagate", "routine", "stabilize",
+        "upgrade", "/hwgw/weaken2", "/hwgw/grow", "/hwgw/hack", "/hwgw/weaken"]
 
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/hwgw.js", "hwgw.js")
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/hwgw/hack.js", "/hwgw/hack.js")
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/hwgw/weaken.js", "/hwgw/weaken.js")
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/hwgw/grow.js", "/hwgw/grow.js")
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/hwgw/weaken2.js", "/hwgw/weaken2.js")
-
-	await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/git.js", "git.js")
+	for (let script of scripts.map(script => script + ".js"))
+		await ns.wget("https://raw.githubusercontent.com/JaGennis/bitburner-scripts/main/" + script, script)
 }
