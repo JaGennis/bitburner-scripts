d73cadb28506bc9c996f1e36f90fb695fa9e6b96
Jannis Geuppert <jannis.n.geuppert@stud.h-da.de>
2022-03-29 03:30:25 +0200

    Minor changes in spiralize-matrix.hs

--- contracts/spiralize-matrix.hs
+++ contracts/spiralize-matrix.hs
@@ -1,24 +1,27 @@
 #!/usr/bin/env runhaskell
 import System.Environment ( getArgs )
 
-trim = filter (\x -> x /= ' ')
+trim = filter (/= ' ')
+
+tail' [] = []
+tail' x = tail x
 
 spiralizeMatrix :: Eq a => [[a]] -> [a]
 spiralizeMatrix [[]] = []
-spiralizeMatrix [x]  = x
-spiralizeMatrix matrix = if (or $ map null matrix)
+spiralizeMatrix [[x]] = [x]
+spiralizeMatrix matrix = if any null matrix
                          then []
-                         else top ++ right ++ bottom ++ left ++ (spiralizeMatrix rest)
+                         else top ++ right ++ bottom ++ left ++ spiralizeMatrix rest
     where top    = head matrix
-          right  = map last . tail . init $ matrix
+          right  = map last . tail' . init $ matrix
           bottom = reverse . last $ matrix
-          left   = reverse . map head . tail . init $ matrix
-          rest   = map (tail . init)  . tail . init $ matrix
+          left   = reverse . map head . tail' . init $ matrix
+          rest   = map (tail' . init)  . tail' . init $ matrix
 
 main :: IO ()
 main = do
     filename  <- head <$> getArgs
     rawMatrix <- readFile filename
-    let fixMatrix = trim . init . concat . map (\x -> x ++ " ,") . tail . init . lines
-    let fixedMatrix = read ("[" ++ (fixMatrix rawMatrix) ++ "]") :: [[Int]]
+    let fixMatrix = trim . init . concatMap (++ " ,") . tail . init . lines
+    let fixedMatrix = read ("[" ++ fixMatrix rawMatrix ++ "]") :: [[Int]]
     print $ spiralizeMatrix fixedMatrix
