54d920e7eafbbd9495638f637f410a77a5b47a0d
JaGennis <JaGennis@users.noreply.github.com>
2022-04-06 03:28:06 +0200

    Added comment to make pure

--- contracts.js
+++ contracts.js
@@ -1,6 +1,7 @@
 /** @param {NS} ns **/
 import { getAllServers } from "helper-functions.js";
 export async function main(ns) {
+    // TODO : Make pure
 	getAllServers(ns)
         .filter(item => ns.ls(item, "contract").length > 0 && item !== "home")
 	    .map(server => ns.tprint (server + " : " + ns.ls(server, "contract")
