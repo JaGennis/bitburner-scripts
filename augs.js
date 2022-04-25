4ce5745f0079c0d3e776c2c6a7cfc1810c45df90
JaGennis <JaGennis@users.noreply.github.com>
2022-04-25 23:22:30 +0200

    Show only buyable augmentations

--- augs.js
+++ augs.js
@@ -2,7 +2,11 @@
 export async function main(ns) {
 	const allFactions = ns.getPlayer().factions
 	const allAugs     = allFactions.map(faction => ns.getAugmentationsFromFaction(faction)).flat()
-	const uniqueAugs  = [... new Set(allAugs)]
+	const buyableAugs = allFactions .map(faction => ns.getAugmentationsFromFaction(faction)
+		.filter(aug => ns.getAugmentationRepReq(aug) <= ns.getFactionRep(faction)))
+		.flat()
+	// const uniqueAugs  = [... new Set(allAugs)]
+	const uniqueAugs  = [... new Set(buyableAugs)]
 	const sortedAugs  = uniqueAugs.sort((a,b) => ns.getAugmentationPrice(a) - ns.getAugmentationPrice(b))
 	const unownedAugs = uniqueAugs.filter(aug => !ns.getOwnedAugmentations().includes(aug))
 	unownedAugs.map(aug => ns.tprint(aug + " : " + ns.getAugmentationPrice(aug) + " : "
