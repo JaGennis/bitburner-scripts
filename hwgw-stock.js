6ea95fa30da0a34a2bdedda016bffd3b17381ba6
JaGennis <JaGennis@users.noreply.github.com>
2022-05-08 11:40:22 +0200

    Completely reworked hwgw-stock.js

--- hwgw-stock.js
+++ hwgw-stock.js
@@ -27,60 +27,55 @@ export async function main(ns) {
 		}
 	}
 
-	async function growMax(targetServer) {
+	async function growMax(targetServer, affectStock) {
+		if (ns.getServerMaxMoney(targetServer) == ns.getServerMoneyAvailable(targetServer))
+			hackMax(targetServer, false)
+
 		const delay = 200
-		while (ns.getServerMaxMoney(targetServer) > ns.getServerMoneyAvailable(targetServer)) {
 	
-			const bestServer = getBestServer(ns)
-			const cores = ns.getServer(bestServer).cores
-	
-			ns.print("Best Server: " + bestServer + " with " + getFreeRam(ns, bestServer) + " GB RAM")
-	
-			let metaWeak = { time: ns.getWeakenTime(targetServer), threads: 1, script: "/hwgw/weaken.js" }
-			let metaGrow = { time: ns.getGrowTime(targetServer) + delay, threads: 1, script: "/stock/grow.js" }
-	
-			await ns.scp(metaWeak.script, bestServer)
-			await ns.scp(metaGrow.script, bestServer)
-			await ns.scp("/hwgw/dummy.js", bestServer)
-	
-			const percentNeeded = 1 / ((1 + ns.getServerMoneyAvailable(targetServer)) / ns.getServerMaxMoney(targetServer))
-			while (metaGrow.threads < ns.growthAnalyze(targetServer, percentNeeded, cores)) {
-				let newGrowThreads = metaGrow.threads + 1
-				const growSecIncrease = ns.growthAnalyzeSecurity(newGrowThreads)
-	
-				let newWeakenThreads = metaWeak.threads
-				while (growSecIncrease - ns.weakenAnalyze(newWeakenThreads, cores) > 0)
-					newWeakenThreads++
-	
-				if (ns.getScriptRam(metaWeak.script) * newWeakenThreads
-					+ ns.getScriptRam(metaGrow.script) * newGrowThreads
-					> getFreeRam(ns, bestServer))
-					break
-				else {
-					metaWeak.threads = newWeakenThreads
-					metaGrow.threads = newGrowThreads
-				}
-			}
-	
-			const sorted = [metaWeak, metaGrow].sort(function (a, b) { return b.time - a.time })
-	
-			const dummyPID = ns.exec("/hwgw/dummy.js", bestServer, sorted[1].threads, targetServer)
-	
-			ns.exec(sorted[0].script, bestServer, sorted[0].threads, targetServer)
-			await ns.sleep(sorted[0].time - sorted[1].time)
-			
-			ns.kill(dummyPID)
-			ns.exec(sorted[1].script, bestServer, sorted[1].threads, targetServer)
-			await ns.sleep(sorted[1].time)
-	
-			await ns.sleep(100)
+		const bestServer = getBestServer(ns)
+		const cores = ns.getServer(bestServer).cores
+
+		ns.print("Best Server: " + bestServer + " with " + getFreeRam(ns, bestServer) + " GB RAM")
+
+		let metaWeak = { time: ns.getWeakenTime(targetServer), threads: 1, script: "/hwgw/weaken.js" }
+		let metaGrow = { time: ns.getGrowTime(targetServer) + delay, threads: 1, script: "/hwgw/grow.js" }
+		if (affectStock)
+			metaGrow.script = "/stock/grow.js"
+
+		await ns.scp(metaWeak.script, bestServer)
+		await ns.scp(metaGrow.script, bestServer)
+		await ns.scp("/hwgw/dummy.js", bestServer)
+
+		while (ns.getScriptRam(metaGrow.script) * (metaGrow.threads + 1) 
+			 + ns.getScriptRam(metaWeak.script) * metaWeak.threads
+			   < getFreeRam(ns, bestServer)) {
+			metaGrow.threads++
+			while (ns.growthAnalyzeSecurity(metaGrow.threads) - ns.weakenAnalyze(metaWeak.threads, cores) > 0)
+				metaWeak.threads++
 		}
+
+		const sorted = [metaWeak, metaGrow].sort(function (a, b) { return b.time - a.time })
+
+		const dummyPID = ns.exec("/hwgw/dummy.js", bestServer, sorted[1].threads, targetServer)
+
+		ns.exec(sorted[0].script, bestServer, sorted[0].threads, targetServer)
+		await ns.sleep(sorted[0].time - sorted[1].time)
+		
+		ns.kill(dummyPID)
+		ns.exec(sorted[1].script, bestServer, sorted[1].threads, targetServer)
+		await ns.sleep(sorted[1].time)
 	}
 
-	async function hackMax(targetServer) {
+	async function hackMax(targetServer, affectStock) {
+		if (ns.getServerMoneyAvailable(targetServer) <= 0)
+			growMax(targetServer, false)
+
 		const delay = 200
-		let metaHack = {time:ns.getHackTime(targetServer) + delay, threads:1, script:"/stock/hack.js"}
+		let metaHack = {time:ns.getHackTime(targetServer) + delay, threads:1, script:"/hwgw/hack.js"}
 		let metaWeak = {time:ns.getWeakenTime(targetServer), threads:1, script:"/hwgw/weaken.js"}
+		if (affectStock)
+			metaHack.script = "/stock/hack.js"
 
 		const bestServer = getBestServer(ns)
 		const cores = ns.getServer(bestServer).cores
@@ -91,10 +86,9 @@ export async function main(ns) {
 		await ns.scp(metaWeak.script, bestServer)
 		await ns.scp("/hwgw/dummy.js", bestServer)
 
-		while (ns.hackAnalyze(targetServer) * (metaHack.threads + 1 ) < 1 
-		       && (ns.getScriptRam(metaHack.script) * (metaHack.threads + 1) 
-			   	+ ns.getScriptRam(metaWeak.script) * metaWeak.threads)
-			   > getFreeRam(ns, bestServer)) {
+		while (ns.getScriptRam(metaHack.script) * (metaHack.threads + 1) 
+			 + ns.getScriptRam(metaWeak.script) * metaWeak.threads
+			   < getFreeRam(ns, bestServer)) {
 			metaHack.threads++
 			while (ns.hackAnalyzeSecurity(metaHack.threads) - ns.weakenAnalyze(metaWeak.threads, cores) > 0)
 				metaWeak.threads++
@@ -123,19 +117,24 @@ export async function main(ns) {
 	await weakenMax(targetServer)
 
 	while (true) {
+		const buyPrice = ns.stock.getAskPrice(stockSym)
 		buyMax(ns, stockSym)
-
-		await growMax(targetServer)
-
 		await ns.sleep(6000)
+
+		while (ns.stock.getBidPrice(stockSym) < buyPrice * 1.10)
+			await growMax(targetServer, true)
+
 		ns.stock.sell(stockSym, ns.stock.getPosition(stockSym)[0])
 
 		await ns.sleep(6000)
+		
+		const sellPrice = ns.stock.getBidPrice(stockSym)
 		shortMax(ns, stockSym)
-
-		await hackMax(targetServer)
-
 		await ns.sleep(6000)
+
+		while (ns.stock.getAskPrice(stockSym) < sellPrice * 1.10)
+			await hackMax(targetServer, true)
+
 		ns.stock.sellShort(stockSym, ns.stock.getPosition(stockSym)[0])
 
 		await ns.sleep(6000)
